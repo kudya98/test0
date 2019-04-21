9c9ffede48f1b7f7e8d72cb5359e9a39837b6fb3
kudya98 <kudyastep98@gmail.com>
2019-04-21 22:28:26 +0700

     

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -12,18 +12,18 @@ const todos = (state = [], action) => {
                 ...state
             ]
         case 'TOGGLE_TODO':
-            let new_todos = state.map(todo =>
+            state = state.map(todo =>
                 (todo.id === action.id)
                     ? {...todo, completed: !todo.completed}
                     : todo
             )
-            let completed_todos=new_todos.filter(todo => (!todo.completed)).
+            let completed_todos=state.filter(todo => (!todo.completed)).
             sort((a,b) => new Date(b.date) - new Date(a.date))
-            let incomplete_todos=new_todos.filter(todo => (todo.completed)).
+            let incomplete_todos=state.filter(todo => (todo.completed)).
             sort((a,b) => new Date(b.date) - new Date(a.date))
             return completed_todos.concat(incomplete_todos)
         case 'REMOVE_TODO':
-            return state.filter(todo => (todo.id !== action.id) )
+            return state.filter(todo => (todo.id !== action.id))
         case 'CLEAR_LIST':
             return []
         case 'CLEAR_COMPLETED':
